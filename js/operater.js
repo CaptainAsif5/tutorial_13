80a034dfe4b039566debd5879079d95aeaadee01
asif khan <asifkhandeveloper4@gmail.com>
2023-09-17 01:05:36 +0500

    this is increment and decrement system basic

---
+++ js/operater.js
@@ -0,0 +1,60 @@
+// Unary Operater
+
+// ++ (increment) by 1, --(Decrement) by 1
+//     post increment/decrement and pre increment/decrement
+
+var num1 = 5;
+document.write("Number is : " + num1 + "<br>");
+
+//Unary Operater
+document.write("Number after increment : " + ++num1 + "<br>");
+document.write("Number after decrement : " + --num1 + "<br>");
+document.write("Number after decrement : " + --num1 + "<br>");
+document.write("Number after increment : " + ++num1 + "<br>");
+
+//Relational operater
+
+//assignment operater "=", Greater then ">",Less then "<",
+// Greater then equals to ">=" , Less then euqals to "<=" , Equals to "==" ,
+// Not Equals to "!=" , Strictly equals to "===" , Not strictly equals to "!=="
+
+var num2 = 5;
+document.write("Number is : " + num2 + "<br>");
+document.write("Number after post inc : " + num2 + "<br>");
+document.write("Number on next line : " + num2 + "<br>");
+
+//Relational operater
+var num3 = 4;
+document.write(++num3 + num3+++ "<br>");
+
+//Relational operater
+var num4 = 6;
+var num5 = 8;
+document.write((num4 < num5) + "<br>");
+document.write((num4 > num5) + "<br>");
+document.write((num4 >= num5) + "<br>");
+document.write((num4 <= num5) + "<br>");
+document.write((num4 == num5) + "<br>");
+document.write((num4 == num5) + "<br>");
+document.write((num4 == num5) + "<br>");
+
+//Relational operater
+var num6 = 6;
+var num7 = '8';
+document.write((num6 === num7) + "<br>");
+document.write((num6 !== num7) + "<br>");
+
+//Ternary Operator
+//Change boolen 0/1 into own choice message
+
+var age = 17;
+var result = age >= 18 ? "Eligible" : "Not Eligible";
+document.write(result + "<br><br>");
+
+// Logical Operater / Conditional Operater
+// Class 14
+
+
+
+//Conditional Statement
+//Class 14
